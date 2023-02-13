8f90af2605d46fbdc40dfd49d5c2b7dbe065d289
PMahhov <petermahhov@gmail.com>
2023-02-13 14:35:09 +0100

    added basic adjacency dictionary

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import networkx
 import pydot
 G = networkx.Graph(networkx.nx_pydot.read_dot('data/LesMiserables.dot'))
 
-for n in G.nodes():
-    print(f"Found node with id {n}")
-for e in G.edges():
+print("Data loaded")
 
+adjacency_dict = {}
+
+for n in G.nodes():
+    print(f"Added node with id {n}")
+    adjacency_dict[n] = []
+
+# the les miserables import has a node with id '\\n' and no connections
+
+for e in G.edges():
     u, v = e
-    print(f"Edge from {u} to {v}")
-    print(G.edges('0')) # Handy functionality =)
-    print(G.edges('1'))
+    adjacency_dict[u].append(v)
+    adjacency_dict[v].append(u)
+    print(f"Added edge from {u} to {v}")
+
+
+#print(G.edges('0'))
+print(adjacency_dict)
