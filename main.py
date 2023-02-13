4ad2524048bac6587b0224407582fb290c394ced
HBekteshi <habibbekteshi@hotmail.com>
2023-02-13 10:39:47 +0100

    main update

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-#import networkx
-import requests
+import networkx
+import pydot
+G = networkx.Graph(networkx.nx_pydot.read_dot('data/LesMiserables.dot'))
 
-response = requests.get('https://httpbin.org/ip')
+for n in G.nodes():
+    print(f"Found node with id {n}")
+for e in G.edges():
 
-print('Your IP is {0}'.format(response.json()['origin']))
-
-
-# G = networkx.Graph(networkx.nx_pydot.read_dot('LesMiserables.dot'))
-
-# for n in G.nodes():
-#     print(f"Found node with id {n}")
-# for e in G.edges():
-
-#     u, v = e
-#     print(f"Edge from {u} to {v}")
-#     print(G.edges('0')) # Handy functionality =)
-#     print(G.edges('1'))
+    u, v = e
+    print(f"Edge from {u} to {v}")
+    print(G.edges('0')) # Handy functionality =)
+    print(G.edges('1'))
