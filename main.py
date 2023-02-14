b5c2912924d1caadbf878b072d354a8470d678af
Lisanne Koetsier <Lisannekoetsier1999@gmail.com>
2023-02-14 21:19:07 +0100

    added start of the solar system, not finished yet

--- main.py
+++ main.py
@@ -21,3 +21,29 @@ for e in G.edges():
 
 #print(G.edges('0'))
 print(adjacency_dict)
+
+
+def create_coordinates(width, height, adjancency_dict):
+    coordinates = {}
+    adjacencies = adjacency_dict 
+
+    #if dictionary is empty return the coordinates
+    if(not adjacencies):
+        return coordinates
+
+    for id, adj_nodes in adjacencies: #create dictionary with the size of the number of edges per node
+        adjacencies.update(id=len(adj_nodes))
+
+    max_adjancency = max(adjacencies.values()) #retrieve the max adjacency
+    max_adjacencies = [n for n,v in adjacencies.items() if v == max_adjancency] #make list of all nodes with the max adjacency
+
+    if(len(max_adajacencies) == 1): #still have to figure out how to do this part exactly with determining rings/coordinates etc.
+        ...
+    else:
+        ...
+
+    #generate dictionary without the keys from the previous maximum adjacencies
+    for id in max_adjacencies:
+        adjacencies.pop(key)
+    
+    create_coordinates(width, heigh, adjacencies) #recurse until empty dictionary
