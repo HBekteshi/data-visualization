738b1a7e59cf3d4115d34e93be4ac0fcac2c3b9c
Lisanne Koetsier <Lisannekoetsier1999@gmail.com>
2023-02-16 19:58:36 +0100

    Add assign to ring functionality

--- main.py
+++ main.py
@@ -29,24 +29,24 @@ print(adjacency_dict)
 
 def create_random_coordinates(width, height):
     coordinates = {}
-    for n in adjacency_dict.keys():
+    for n in G.nodes():
         x_val = np.random.uniform(-width/2, width/2)
         y_val = np.random.uniform(-height/2, height/2)
         coordinates[n] = (x_val, y_val)
     return coordinates
 
 random_coordinates = create_random_coordinates(100, 100)
-print(random_coordinates)
 
 def create_coordinates(width, height, adjancency_dict):
     coordinates = {}
     adjacencies = adjacency_dict 
 
-    for id, adj_nodes in adjacencies: #create dictionary with the size of the number of edges per node
-        adjacencies.update(id=len(adj_nodes))
+    for id, adj_nodes in list(adjacencies.items()): #create dictionary with the size of the number of edges per node
+        adjacencies[id] = len(adj_nodes)
 
     max_adjancency = max(adjacencies.values()) #retrieve the max adjacency
-    nr_rings = len(set(adjacencies.values())) #calculate # of rings based on the # of unique values
+    max_adjacencies = [n for n,v in adjacencies.items() if v == max_adjancency] #make a list of all the nodes with the max adjacency
+    rings_dict = assign_to_rings(adjacencies, max_adjancency)
 
     if(len(max_adjacencies) == 1): #still have to figure out how to do this part exactly with determining rings/coordinates etc.
         ...
@@ -70,6 +70,23 @@ def create_coordinates(width, height, adjancency_dict):
 
     return coordinates
 
+def assign_to_rings(adjacencies, max_adjancency):
+
+    #create dictionary with rings, based on the # of rings
+    nr_rings = len(set(adjacencies.values())) #calculate # of rings based on the # of unique values in adjacency numbers
+    rings_dict = {}
+    for ring in set(adjacencies.values()): #create a ring for each unique value in the adjacencies, with that key value as the key
+        rings_dict[ring] = []
+
+    #for every node, add it to the ring with the corresponding size id
+    for node, size in adjacencies.items(): 
+        rings_dict[size].append(node)
+
+    return rings_dict
+
+
 def calc_radius(height, width, size):
     #calculate the radius of a ring based on the height, width and number of nodes on the ring 
     return
+
+#coordinates = create_coordinates(100,100, adjacency_dict)
